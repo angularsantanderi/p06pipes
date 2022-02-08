fb7e0829fe18e96552069bc99bfcd71c4f8f5774
angularsantanderi <angularsantanderi@gmail.com>
2022-02-08 09:28:33 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'p06pipes';
+  texto: string = 'ciudad de Madrid';
+  id: any = 1;
 }
